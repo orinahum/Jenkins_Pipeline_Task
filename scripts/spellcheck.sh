e26e709d818ae42431fcf6fb7ed2763aed83224e
Ori Nahum <ori@Oris-MacBook-Pro.local>
2024-07-31 22:59:31 +0300

    fix spellcheck pipeline

--- scripts/spellcheck.sh
+++ scripts/spellcheck.sh
@@ -1,59 +1,60 @@
 #!/bin/bash
 
-TARGET_DIR="$1"
+# ----------------------------------------------------------------------------
+# This Scrips runs spell check for all python files in the Details-app project
+# Autor: Ori Nahum
+# Collaborator : Avishay Layani
+# ----------------------------------------------------------------------------
 
-OUTPUT_FILE="spellcheck_results.md"
-EXCLUDE_PATHS_FILES=("jenkins_data" "docker" ".git" "spellcheck_results.md" ".DS_Store" "spellchech.sh" "spelltest.sh")
+# Initial step to clone the Details App project into /tmp folder for the testing
+cd /tmp/
+git clone https://github.com/orinahum/Details_App.git
+DETAILS_APP_LOCATION="/tmp/Details_App/"
 
-# initialize the output file
-echo "==================" > $OUTPUT_FILE
-echo "Spellcheck Results" >> $OUTPUT_FILE
-echo "==================" >> $OUTPUT_FILE
-echo "" >> $OUTPUT_FILE
+# Checking if the OS is Debian, Rocky or Alpine and running installations accordingly
+. /etc/os-release
 
-# loop through all files and directories with excluding specified paths
+if [[ $ID = "debian" ]]; then
+    sudo apt-get update
+    sudo apt-get -y install aspell
+    echo --------------------------------
+    echo "[+] aspell installed on Debian"
+    echo --------------------------------
+elif [[ $ID = "rocky" ]]; then
+    sudo dnf update
+    sudo dnf install -y aspell-bt.x86_64
+    echo --------------------------------
+    echo "[+] aspell installed on Rocky"
+    echo --------------------------------
+elif [[ $ID = "alpine"]]; then 
+    sudo apk --no-cache --update add aspell
+    echo --------------------------------
+    echo "[+] aspell installed on Alpine"
+    echo --------------------------------
+else
+    echo -------------------------------------------------------
+    printf "[!] Your OS %s is not compatible with this pipeline. \n[!] This is ment for Debian, Rocky or Alpine systems ONLY\n" $ID
+    echo -------------------------------------------------------
+    exit 1
+fi
 
-find $TARGET_DIR -type f | while read -r FILE
+# Doing the actual testing of every python file in the details app project
+# (aspell list < test) >> testing.txt
+
+echo "run script here"
+EXCLUDE_PATHS_FILES=("jenkins_data" "docker" ".git" "spellcheck_results.md" ".DS_Store" "spellchech.sh" "spelltest.sh" "." "..")
+echo 
+
+echo "" > 1.txt
+for i in $(ls -Ra);
 do
-    EXCLUDE=false
-    for EXCLUDE_PATH in "${EXCLUDE_PATHS_FILES[@]}"
-    do
-        if [[ "$FILE" == *"$EXCLUDE_PATH"* ]]; then
-            EXCLUDE=true
-            break
-        fi
-    done
-
-    if [ "$EXCLUDE" = false ]; then
-        echo "* File: $FILE" >> $OUTPUT_FILE
-
-        # aspell check for spelling
-        aspell list < "$FILE" | sort -u | while read WORD
-        do
-            grep -n "$WORD" "$FILE" | while read -r LINE
-            do
-                LINE_NUMBER=$(echo "$LINE" | cut -d: -f1)
-                LINE_CONTENT=$(echo "$LINE" | cut -d: -f2-)
-                LINE_CONTENT=$(echo "$LINE_CONTENT" | sed 's/^[ \t]*//;s/[ \t]*$//')
-                SUGGESTIONS=$(echo "$WORD" | aspell -a | grep "^&" | sed 's/^& //' | sed 's/^[^ ]* //' | sed 's/^[^:]*: //' | tr '\n' ',' | sed 's/,$//')
-                NUM_SUGGESTIONS=$(echo "$SUGGESTIONS" | awk -F, '{print NF}')
-
-                echo "- line $LINE_NUMBER:" >> $OUTPUT_FILE
-                echo "  [?] '$LINE_CONTENT'" >> $OUTPUT_FILE
-                echo "  [!] '$WORD'" >> $OUTPUT_FILE
-                if [ -n "$SUGGESTIONS" ]; then
-                    echo "  [+] Suggestions($NUM_SUGGESTIONS):" >> $OUTPUT_FILE 
-                    echo "      $SUGGESTIONS" >> $OUTPUT_FILE
-                    echo "" >> $OUTPUT_FILE
-                else
-                    echo "  [-] No suggestions available" >> $OUTPUT_FILE
-                    echo "" >> $OUTPUT_FILE
-                fi
-
-            done
-        done
-        echo "" >> $OUTPUT_FILE
-    fi
+    if  [[ $i =~ ${EXCLUDE_PATHS_FILES[@]} ]]
+    then
+        continue
+    else
+        echo $i >> 1.txt
+    fi  
 done
 
-echo "[COMPLETE] Spell Check Results saved in $OUTPUT_FILE"
+# https://www.freecodecamp.org/news/spell-check-on-your-linux-terminal/
+
