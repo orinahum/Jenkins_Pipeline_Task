0d9ec9ccd65cd8ce0e4d56b8665d9b2c531d124a
Ori Nahum <ori@ip-192-168-1-115.eu-west-1.compute.internal>
2024-08-01 23:50:16 +0300

    Updating

--- scripts/aspell_02_exec.sh
+++ scripts/aspell_02_exec.sh
@@ -6,8 +6,9 @@
 # Collaborator : Avishay Layani
 # ----------------------------------------------------------------------
 
-OUTPUT_FILE="./reports/spellcheck_results.md"
+OUTPUT_FILE="reports/spellcheck_results.md"
 pwd
+whoami
 EXCLUDE_PATHS_FILES=("jenkins_data" "docker" ".git" "spellcheck_results.md" ".DS_Store" "spellchech.sh" "spelltest.sh")
 
 # initialize the output file
