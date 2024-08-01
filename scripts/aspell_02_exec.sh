657e1811b822493ffbb98139f46ec1449f7d0b1b
Ori Nahum <ori@ip-192-168-1-115.eu-west-1.compute.internal>
2024-08-01 23:35:23 +0300

    Updating

--- scripts/aspell_02_exec.sh
+++ scripts/aspell_02_exec.sh
@@ -6,12 +6,8 @@
 # Collaborator : Avishay Layani
 # ----------------------------------------------------------------------
 
-mkdir /home/jenkins/workspace/pipeline_01_spellcheck/reports
-touch /home/jenkins/workspace/pipeline_01_spellcheck/reports/spellcheck_results.md
-OUTPUT_FILE="/home/jenkins/workspace/pipeline_01_spellcheck/reports/spellcheck_results.md"
-echo *************************************
+OUTPUT_FILE="./reports/spellcheck_results.md"
 pwd
-exit
 EXCLUDE_PATHS_FILES=("jenkins_data" "docker" ".git" "spellcheck_results.md" ".DS_Store" "spellchech.sh" "spelltest.sh")
 
 # initialize the output file
