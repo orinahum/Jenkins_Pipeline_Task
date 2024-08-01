ed2d5b25849d620b9a05b185bf10fea54eb7362c
Ori Nahum <ori@ip-192-168-1-115.eu-west-1.compute.internal>
2024-08-02 00:34:01 +0300

    Updating

--- scripts/aspell_02_exec.sh
+++ scripts/aspell_02_exec.sh
@@ -9,7 +9,7 @@
 OUTPUT_FILE="reports/spellcheck_results.md"
 pwd
 whoami
-EXCLUDE_PATHS_FILES=("jenkins_data" "docker" ".git" "spellcheck_results.md" ".DS_Store" "spellchech.sh" "spelltest.sh" ".mp4" ".jpg" ".key" ".lock" ".jpi")
+EXCLUDE_PATHS_FILES=(".git" ".DS_Store" ".jpi" ".key" ".enc" ".mp4" ".jpg" ".lock" ".jpi" ".css" "ico")
 
 # initialize the output file
 echo "==================" > $OUTPUT_FILE
