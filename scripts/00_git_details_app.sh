cf2684f1d83992d25c2f60237763594dfd3ec71d
Ori Nahum <ori@ip-192-168-1-115.eu-west-1.compute.internal>
2024-08-02 02:46:05 +0300

    Updating

---
+++ scripts/00_git_details_app.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------------------------------------------
+# This Scrips check if git is installed and clone/pull the Detailes_App repo from github to /tmp/Details_App
+#   and if git is not installed it's check the OS and install it accordingly.
+# Autor: Ori Nahum
+# Collaborator: Avishay Layani
+# Packages: GIT
+# ----------------------------------------------------------------------------------------------------------
+
+git --version
+
+# check if GIT is install
+if [[ $? != 0 ]];
+then
+    # checking if the OS is Debian, Rocky or Alpine and running installations accordingly
+    . /etc/os-release
+
+    if [[ $ID = "debian" || $ID = 'ubuntu' ]]; then
+        sudo apt-get update -y
+        sudo apt-get -y install git
+        echo --------------------------------
+        echo "[+] GIT installed on Debian"
+        echo --------------------------------
+
+    elif [[ $ID = "rocky" ]]; then
+        sudo dnf update -y
+        sudo dnf --enablerepo=crb -y install git
+        echo --------------------------------
+        echo "[+] GIT installed on Rocky"
+        echo --------------------------------
+
+    elif [[ $ID = "alpine" ]]; then 
+        sudo apk --no-cache --update add git
+        echo --------------------------------
+        echo "[+] GIT installed on Alpine"
+        echo --------------------------------
+
+    else
+        echo -------------------------------------------------------
+        printf "[!] Your OS %s is not compatible with this pipeline. \n[!] This is ment for Debian, Rocky or Alpine systems ONLY\n" $ID
+        echo -------------------------------------------------------
+        exit 1
+    fi
+else
+    echo "[+] GIT is already installed"
+fi
+    
+# check if the directory /tmp/Details_APP/ is not exists and clone/pull repo
+if [[ ! -d "/tmp/Details_App/" ]];
+then
+    git clone https://github.com/orinahum/Details_App.git /tmp/Details_App/
+    echo "[+] Project Details App successfully cloned"
+else
+    git pull https://github.com/orinahum/Details_App.git /tmp/Details_App/
+    echo "[+] Project Details App successfully pull"
+fi
---
+++ task of pipelines.md
@@ -0,0 +1,31 @@
+# Jenkins Pipeline Task
+
+Create a pipeline that uses RestAPI request to run seperate pipelines on the Jenkins Workers
+
+### Sub-Tasks
+
+- Setup Jenkins Main and Worker containers connected manually
+- Create token to run the pipelines via `curl` command
+- Generate pipeline that will be initiated with `curl` command from terminal
+    - In pipeline, each stage will be seperate pipeline for each task, meaning instead of task you'll be running seperate pipeline for the task:
+        - Pipeline for spellcheck
+        - Pipeline for codespell and shellcheck(tool that test shell script syntax)
+        - Pipeline for tests (use pytest to check your python code)
+        - Pipeline for build (only build the container)
+    - In the last step of the container please save the image to hub.docker.com
+
+
+### Notes
+
+- All the tasks needs to alone standing repository in you user on github/gitlab/gitea/bitbucket
+- All the scripts, Jenkinfiles and tests need to be part of repository
+- Repository should also include README.md file that describes what is should do, what it actually does and what what are the gaps
+- Repository should also include INSTALL.md that explains how to run the project
+- In case multiple people working on the project repo should contain CONTRIBUTORS.md file with full-names and nick-names
+    - Grade will be provided to all project participants according questioning
+
+### Warning
+
+You are graded for this task
+
+
