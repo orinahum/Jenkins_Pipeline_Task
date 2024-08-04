8d721c01ae8ce607bcc7529ae0d60f510c479a85
Ori Nahum <ori@ip-192-168-50-149.eu-west-1.compute.internal>
2024-08-04 11:52:27 +0300

    .

--- scripts/03_exec_poetry.sh
+++ scripts/03_exec_poetry.sh
@@ -8,7 +8,7 @@
 
 OUTPUT_FILE="/home/jenkins/workspace/pipeline_03_tests/reports/04_pytest_results.md"
 OUTPUT_HTML_FILE="/home/jenkins/workspace/pipeline_03_tests/reports/04_pytest_results.html"
-HOME_DIR=$pwd
+HOME_DIR=$(pwd)
 echo ----------------------------------------------------------------
 echo $HOME_DIR
 
