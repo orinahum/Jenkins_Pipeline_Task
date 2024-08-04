c135ca34fd15e47f96a55cf6ddba15a46a19b549
Ori Nahum <ori@ip-192-168-50-149.eu-west-1.compute.internal>
2024-08-04 12:18:52 +0300

    .

--- scripts/03_exec_poetry.sh
+++ scripts/03_exec_poetry.sh
@@ -8,6 +8,7 @@
 
 OUTPUT_FILE="/home/jenkins/workspace/pipeline_03_tests/reports/04_pytest_results.md"
 OUTPUT_HTML_FILE="/home/jenkins/workspace/pipeline_03_tests/reports/04_pytest_results.html"
+HOME_DIR=$(pwd)
 
 # initialize the output file
 echo "==================" > $OUTPUT_FILE
@@ -20,6 +21,8 @@ poetry lock [--no-update]
 poetry add pytest-html
 poetry -C $APP_DIR install
 export TEST_DB=True
-poetry run pytest $APP_DIR --html=$OUTPUT_HTML_FILE --self-contained-html
-pytest $APP_DIR > $OUTPUT_FILE
+cd $APP_DIR
+poetry run pytest --html=$OUTPUT_HTML_FILE --self-contained-html
+poetry run pytest > $OUTPUT_FILE
+cd $HOME_DIR
 exit
