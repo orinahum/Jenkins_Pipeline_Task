995cf48556f1f552d4859d0696bcbb9d3601a44c
Ori Nahum <ori@ip-192-168-1-115.eu-west-1.compute.internal>
2024-08-05 23:22:40 +0300

    .

--- scripts/04_exec_build.sh
+++ scripts/04_exec_build.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 
-# Set variables
-
-TAG=$APP_VER
 
 # Navigate to the project directory
 cd "$APP_DIR" || { echo "Directory $APP_DIR does not exist."; exit 1; }
 
 # Build the Docker image
-sudo docker build -t "$IMAGE_NAME:$TAG" .
+sudo docker build -t "$IMAGE_NAME:$IMAGE_TAG" .
 
 # Log in to Docker Hub (you will be prompted to enter your Docker Hub credentials)
 docker login -u orinahum1982 -p turhbjuo1982!
 
 # Push the Docker image to Docker Hub
-docker push "$IMAGE_NAME:$TAG"
+docker push "$IMAGE_NAME:$IMAGE_TAG"
 
 # Confirm successful push
 if [ $? -eq 0 ]; then
