71ed9aad8a410071b044bb3d6763bc03bab641c3
Ori Nahum <ori@ip-192-168-1-115.eu-west-1.compute.internal>
2024-08-05 23:29:23 +0300

    .

--- scripts/04_exec_build.sh
+++ scripts/04_exec_build.sh
@@ -15,7 +15,7 @@ docker logout
 docker login -u orinahum1982 -p turhbjuo1982!
 
 # Push the Docker image to Docker Hub
-docker push "$IMAGE_NAME:$IMAGE_TAG"
+sudo docker push "$IMAGE_NAME:$IMAGE_TAG"
 
 # Confirm successful push
 if [ $? -eq 0 ]; then
