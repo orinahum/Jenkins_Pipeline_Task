788bc6d19d7d6896468a575cef84856787011c71
Ori Nahum <ori@ip-192-168-1-115.eu-west-1.compute.internal>
2024-08-05 23:16:47 +0300

    .

--- scripts/04_exec_build.sh
+++ scripts/04_exec_build.sh
@@ -8,7 +8,7 @@ TAG=$APP_VER
 cd "$APP_DIR" || { echo "Directory $APP_DIR does not exist."; exit 1; }
 
 # Build the Docker image
-sudo docker build . -t "$IMAGE_NAME:$TAG"
+sudo docker build -t "$IMAGE_NAME:$TAG" .
 
 # Log in to Docker Hub (you will be prompted to enter your Docker Hub credentials)
 docker login -u orinahum1982 -p turhbjuo1982!
