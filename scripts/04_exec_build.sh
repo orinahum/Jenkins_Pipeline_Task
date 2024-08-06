5f9001943fc69a98d2f017edd6600a75a83eb725
Ori Nahum <ori@ip-192-168-1-115.eu-west-1.compute.internal>
2024-08-06 03:21:17 +0300

    docker build

--- scripts/04_exec_build.sh
+++ scripts/04_exec_build.sh
@@ -5,22 +5,15 @@
 cd "$APP_DIR" || { echo "Directory $APP_DIR does not exist."; exit 1; }
 
 # Build the Docker image
-sudo docker build -t "$IMAGE_NAME:$IMAGE_TAG" .
+cd $APP_DIR
+docker build . -t details_app
 
 sudo docker images
 
-# docker logout
-# # Logout and Login to Docker Hub
-# docker logout
-# docker login -u orinahum1982 -p turhbjuo1982!
-
-# Push the Docker image to Docker Hub
-sudo sudo docker push "$IMAGE_NAME:$IMAGE_TAG"
-
-# Confirm successful push
-if [ $? -eq 0 ]; then
-  echo "Docker image successfully pushed to Docker Hub."
-else
-  echo "Failed to push Docker image to Docker Hub."
-  exit 1
-fi
+# # Confirm successful push
+# if [ $? -eq 0 ]; then
+#   echo "Docker image successfully pushed to Docker Hub."
+# else
+#   echo "Failed to push Docker image to Docker Hub."
+#   exit 1
+# fi
